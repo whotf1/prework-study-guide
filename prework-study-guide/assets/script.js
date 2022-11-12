408cc76c444d56a6cc3ad7d33a7a0205bd2376b0
Milo C <75094742+whotf1@users.noreply.github.com>
2022-11-12 11:03:52 -0600

    added new js code

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,22 +1,30 @@
-var topics = ["HTML","CSS","Git","JavaScript"]
+var topics = ["HTML","CSS","Git","JavaScript"];
+var RndTopic = topics[Math.floor(Math.random()*topics.length)];
 
-for(var x = 0; x < topics.length;x++){
-    console.log(topics[x])
+function ListTopics(){
+    for(var x=0; x<topics.length; x++){
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+
+function SelectTopic(){
+    if (RndTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (RndTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (RndTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (RndTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
+console.log("Topics learned through Prework: ");
+ListTopics();
+console.log("which topic should we learn first");
+SelectTopic();
 
 
 
